a8a10de5b7df0cdf40a429c368068e436fb02f63
Spencer Jack <spencer.jack@alaskaair.com>
2018-11-19 21:19:45 -0800

    adds queryURL ability with decades, and genre's, generic input fields

--- assets/javascript/script1.js
+++ assets/javascript/script1.js
@@ -1,3 +1,9 @@
+/* -------------------------------------------------------------------------------------------------- */
+/*                                                                                                    */
+/*                                        BACKGROUND IMAGES                                           */
+/*                                                                                                    */
+/* -------------------------------------------------------------------------------------------------- */
+
 // Shorthand for $( document ).ready()
 $(function() {
     // Background image array
@@ -78,25 +84,33 @@ $(function() {
 })
 
 /* -------------------------------------------------------------------------------------------------- */
+/*                                                                                                    */
+/*                                             API FUNCTIONS                                          */
+/*                                                                                                    */
 /* -------------------------------------------------------------------------------------------------- */
-/* -------------------------------------------------------------------------------------------------- */
-/* -------------------------------------------------------------------------------------------------- */
-/* -------------------------------------------------------------------------------------------------- */
-/* -------------------------------------------------------------------------------------------------- */
+
+// global used as param in query url
+let pageNum = 1;
+
 
 // captures value(s) from form submission
 $('#my-form').submit(function (event){
     event.preventDefault();
     // grab input value from form
     let movieInput = $('#movie-input').val().trim();
+    /* this is generic */
+    /* will need to be updated when HTML tags include genre id's  */
+    let genreInput = $('#genre-input').val().trim();
+    let decadeInput = $('#decade-input').val().trim();
+
     // pass value to function
     makeApiCall(movieInput);
+    makeApiCallGenre(genreInput, decadeInput);
 });
 
 
-// makes api call with param(s)
 makeApiCall = function(movieInput) {
-    // only param being passed currently is movieInput (a movie title)
+    // movieInput passed as a param to search movie titles
     let queryURL = "https://api.themoviedb.org/3/search/movie?api_key=c5203bcbbee2d69dcb21052d7ef5621c&query=" + movieInput;
 
     $.ajax({ /* jquery ajax call */
@@ -108,25 +122,57 @@ makeApiCall = function(movieInput) {
         });
 }
 
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=c5203bcbbee2d69dcb21052d7ef5621c",
-  "method": "GET",
-  "headers": {},
-  "data": "{}"
-}
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+makeApiCallGenre = function(genreInput, decadeInput) {
+    // variables to access movies within certain decades
+    // passed to queryURL
+    let yearStart = "";
+    let yearEnd = "";
 
+    // define these start and end years using data-tags in the html
+    // in the drop down menu li's 
+    // this is redundant
+    switch (true) {
+        case decadeInput == "1930's":
+            yearStart = "1930",
+            yearEnd = "1949"
+            break
+        case decadeInput == "1940's":
+            yearStart = "1940",
+            yearEnd = "1949"
+            break
+        case decadeInput == "1950's":
+            yearStart = "1950",
+            yearEnd = "1959"
+            break
+        case decadeInput === "1960's":
+            yearStart = "1960",
+            yearEnd = "1969"
+            break
+        case decadeInput === "1970's":
+            yearStart = "1970",
+            yearEnd = "1979"
+            break
+        case decadeInput === "1980's":
+            yearStart = "1980",
+            yearEnd = "1989"
+            break
+        case decadeInput === "1990's":
+            yearStart = "1990",
+            yearEnd = "1999"
+            break
+        case decadeInput === "2000's":
+            yearStart = "2000",
+            yearEnd ="2009"
+            break;
+        case decadeInput === "2010's":
+            yearStart = "2010",
+            yearEnd = "2019"
+            break;
+    }
 
-// makes api call with param(s)
-makeApiCallGenre = function() {
-    // only param being passed currently is movieInput (a movie title)
-    // let queryURL = "https://api.themoviedb.org/3/genre/13/movie/list?language=en-US&api_key=c5203bcbbee2d69dcb21052d7ef5621c";
-    let queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=c5203bcbbee2d69dcb21052d7ef5621c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31&vote_average.gte=6&with_genres=" + genre;
+    // movie genreInput passed as a param to search movie genre's  
+    let queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=c5203bcbbee2d69dcb21052d7ef5621c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + pageNum + "&primary_release_date.gte=" + yearStart + "-01-01&primary_release_date.lte=" + yearEnd + "-12-31&vote_average.gte=6&with_genres=" + genreInput;
 
 
     $.ajax({ /* jquery ajax call */
@@ -134,16 +180,25 @@ makeApiCallGenre = function() {
             method: "GET"
         })
         .then(function (response) { /* promise */
-            console.log(response);
+            console.log(response);            
+            // iterate pageNum for click of 'load more' or 'next page'
+            pageNum++;
         });
 }
 
-makeApiCallGenre();
 
+// to see the list of id's for all genre's
+// not needed for production
 
+// var settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": "https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=c5203bcbbee2d69dcb21052d7ef5621c",
+//   "method": "GET",
+//   "headers": {},
+//   "data": "{}"
+// }
 
-
-
-
-
-// url: "genre/" + options.id + "/movies" + theMovieDb.common.generateQuery(options)
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
