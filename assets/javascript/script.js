461492a49bf2fdfd9619b67bbf27b5a9ee2655a9
mk <42503267+alphamanhu@users.noreply.github.com>
2018-11-27 01:18:58 -0800

    fixed the index increment for zoom's

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -73,18 +73,38 @@ $(function () {
     bgIndex++;
     $('#container-cl-1').find('.inner-div-out').css("background-image", `url('${bgImageArray[bgIndex]}')`);
 
+    // Timer to change background every 5 secs
+    setInterval(() => {
+        if (!flip) {
+            zoomOut("#container-cl-1");
+        } else if (flip) {
+            zoomIn("#container-cl-1");
+        }
+    }, 10000);
+
     /***********Set Container 2**************** */
     $('#container-cl-2').find('.inner-div-in').addClass('active');
 
+    bgIndex++;
     // Pass index of bgImageArray as url link for background image
     $('#container-cl-2').find('.inner-div-in').css("background-image", `url('${bgImageArray[bgIndex]}')`);
 
     bgIndex++;
     $('#container-cl-2').find('.inner-div-out').css("background-image", `url('${bgImageArray[bgIndex]}')`);
 
+    // Timer to change background every 5 secs
+    setInterval(() => {
+        if (!flip) {
+            zoomOut("#container-cl-2");
+        } else if (flip) {
+            zoomIn("#container-cl-2");
+        }
+    }, 10000);
+
     /***********Set Container 3**************** */
     $('#container-cl-3').find('.inner-div-in').addClass('active');
 
+    bgIndex++;
     // Pass index of bgImageArray as url link for background image
     $('#container-cl-3').find('.inner-div-in').css("background-image", `url('${bgImageArray[bgIndex]}')`);
 
@@ -94,12 +114,8 @@ $(function () {
     // Timer to change background every 5 secs
     setInterval(() => {
         if (!flip) {
-            zoomOut("#container-cl-1");
-            zoomOut("#container-cl-2");
             zoomOut("#container-cl-3");
         } else if (flip) {
-            zoomIn("#container-cl-1");
-            zoomIn("#container-cl-2");
             zoomIn("#container-cl-3");
         }
     }, 10000);
@@ -167,21 +183,21 @@ $(function () {
 /* -------------------------------------------------------------------------------------------------- */
 
 // globals used as param in query urls
-let pageNum = 1;
+let page = 1;
 let genreID = "";
 let decadeID = "";
 let i = 0;
+let yearStart = "";
+let yearEnd = "";
 
 // capture input values for genre
 $('#genre').on('click', 'a', function (e) {
     genreID = $(this).attr('id');
-    console.log(genreID);
 })
 
 // capture input values for decade
 $('#decade').on('click', 'a', function (e) {
     decadeID = $(this).attr('id');
-    console.log(decadeID);
 })
 
 
@@ -189,15 +205,14 @@ $('#decade').on('click', 'a', function (e) {
 $('#my-form').submit(function (event) {
     event.preventDefault();
     if ($('#movie-input').val() === "") {
-        let genreInput = genreID; /* change these later to simply genreID and decadeID  revisit*/
-        let decadeInput = decadeID; /* change these later to simply genreID and decadeID  revisit*/
+        // let genreInput = genreID; /* change these later to simply genreID and decadeID  revisit*/
+        // let decadeInput = decadeID; /* change these later to simply genreID and decadeID  revisit*/
 
-        makeApiCallGenre(genreInput, decadeInput);
+        makeApiCallGenre(genreID, decadeID);
         clearBody();
     } else {
         // grab input value from form
         let movieInput = $('#movie-input').val().trim();
-        console.log(movieInput);
         // pass value to function
         makeApiCall(movieInput);
         clearBody();
@@ -213,7 +228,6 @@ clearBody = function () {
 
 
 makeApiCall = function (movieInput) {
-    console.log('make api call ran');
     // movieInput passed as a param to search movie titles
     let queryURL = "https://api.themoviedb.org/3/search/movie?api_key=c5203bcbbee2d69dcb21052d7ef5621c&query=" + movieInput;
     if (movieInput === "") {
@@ -221,9 +235,9 @@ makeApiCall = function (movieInput) {
     }
 
     $.ajax({ /* jquery ajax call */
-        url: queryURL,
-        method: "GET"
-    })
+            url: queryURL,
+            method: "GET"
+        })
         .then(function (response) { /* promise */
             // console.log(response); 
             let data = response;
@@ -232,68 +246,67 @@ makeApiCall = function (movieInput) {
 }
 
 
-makeApiCallGenre = function (genreInput, decadeInput) {
+makeApiCallGenre = function (genreID, decadeID) {
     // variables to access movies within certain decades
     // passed to queryURL
-    let yearStart = "";
-    let yearEnd = "";
+    // let yearStart = "";
+    // let yearEnd = "";
 
     // define these start and end years using data-tags in the html
     // in the drop down menu li's 
     // this is redundant
     switch (true) {
-        case decadeInput == "30's":
+        case decadeID == "30's":
             yearStart = "1930",
                 yearEnd = "1949"
             break
-        case decadeInput == "40's":
+        case decadeID == "40's":
             yearStart = "1940",
                 yearEnd = "1949"
             break
-        case decadeInput == "50's":
+        case decadeID == "50's":
             yearStart = "1950",
                 yearEnd = "1959"
             break
-        case decadeInput === "60's":
+        case decadeID === "60's":
             yearStart = "1960",
                 yearEnd = "1969"
             break
-        case decadeInput === "70's":
+        case decadeID === "70's":
             yearStart = "1970",
                 yearEnd = "1979"
             break
-        case decadeInput === "80's":
+        case decadeID === "80's":
             yearStart = "1980",
                 yearEnd = "1989"
             break
-        case decadeInput === "90's":
+        case decadeID === "90's":
             yearStart = "1990",
                 yearEnd = "1999"
             break
-        case decadeInput === "00's":
+        case decadeID === "00's":
             yearStart = "2000",
                 yearEnd = "2009"
             break;
-        case decadeInput === "10's":
+        case decadeID === "10's":
             yearStart = "2010",
                 yearEnd = "2019"
             break;
     }
 
     // movie genreInput passed as a param to search movie genre's  
-    let queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=c5203bcbbee2d69dcb21052d7ef5621c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + pageNum + "&primary_release_date.gte=" + yearStart + "-01-01&primary_release_date.lte=" + yearEnd + "-12-31&vote_average.gte=6&with_genres=" + genreInput;
+    let queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=c5203bcbbee2d69dcb21052d7ef5621c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + page + "&primary_release_date.gte=" + yearStart + "-01-01&primary_release_date.lte=" + yearEnd + "-12-31&vote_average.gte=6&with_genres=" + genreID;
 
 
     $.ajax({ /* jquery ajax call */
-        url: queryURL,
-        method: "GET"
-    })
+            url: queryURL,
+            method: "GET"
+        })
         .then(function (response) { /* promise */
-            // console.log(response); 
+            console.log(response); 
             let data = response;
             parseData(data);
-            // iterate pageNum for click of 'load more' or 'next page'
-            pageNum++;
+            renderPagination();
         });
 }
 
@@ -313,9 +326,9 @@ parseData = function (data) {
         // constructs percentage colors
         if (vote_average <= 70) {
             vote_color = "orange";
-        } else if (vote_average <= 80) {
+        }else if (vote_average <= 80) {
             vote_color = "green";
-        } else if (vote_average >= 80) {
+        }else if (vote_average >= 80) {
             vote_color = "blue";
         }
         // converts percent into string
@@ -324,15 +337,53 @@ parseData = function (data) {
     });
 }
 
+renderPagination = function() {
+    let pages = $('<div id="pagination"><ul class="pagination pagination-content"><li><a href="#" class="page" id="prev">Prev</a></li><li><a href="#" class="page" id="1">1</a></li><li><a href="#" class="page" id="2">2</a></li><li><a href="#" class="page" id="3">3</a></li><li><a href="#" class="page" id="next">Next</a></li></ul></div>');
+    pages.appendTo('.container-fluid');
+}
+
 renderCard = function (title, release_date, poster, overview, vote_average, vote_color) {
     // $('.container-fluid').empty();
-    var myCol = $('<div class="col-sm-6 col-md-6 pb-4"></div>');
+    let myCol = $('<div class="col-sm-6 col-md-6 pb-4"></div>');
 
-    var myPanel = $('<div class="card card-outline-info" id="' + i + 'Panel"><div class="card-block"> <img src="' + poster + ' "class="rounded poster"> <div class="box"><div class="single-chart"><svg viewBox="0 0 36 36" class="circular-chart ' + vote_color + '"><path class="circle-bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><path class="circle" stroke-dasharray="' + vote_average + ', 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><text x="18" y="20.35" class="percentage">' + vote_average + '%</text></svg><span id="title">' + title + '<br><span id="release-date">' + release_date + '</span></span></div><div id="overview"><p class="overflow">' + overview + ' </p></div></div></div></div>');
+    let myPanel = $('<div class="card card-outline-info" id="' + i + 'Panel"><div class="card-block"> <img src="' + poster + ' "class="rounded poster"> <div class="box"><div class="single-chart"><svg viewBox="0 0 36 36" class="circular-chart ' + vote_color + '"><path class="circle-bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><path class="circle" stroke-dasharray="' + vote_average + ', 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><text x="18" y="20.35" class="percentage">' + vote_average + '%</text></svg><span id="title">' + title + '<br><span id="release-date">' + release_date + '</span></span></div><div id="overview"><p class="overflow">' + overview + ' </p></div></div></div></div>');
 
     myPanel.appendTo(myCol);
     myCol.appendTo('#contentPanel');
     i++;
+}
+
+// capture clicks on pagination buttons
+$(document).on("click",".pagination li a", function(e){
+    e.preventDefault();
+    pageInput = $(this).attr('id');
+    if (pageInput === 'prev') {
+        page = (page - 1);
+    }else if (pageInput === 'next') {
+        page = (page + 1);
+    }
+    console.log(page);
+    goToPage(page);
+});
+
+goToPage = function(page) {
+    
+     // movie genreInput passed as a param to search movie genre's  
+    let queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=c5203bcbbee2d69dcb21052d7ef5621c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + page + "&primary_release_date.gte=" + yearStart + "-01-01&primary_release_date.lte=" + yearEnd + "-12-31&vote_average.gte=6&with_genres=" + genreID;
+
+
+    $.ajax({ /* jquery ajax call */
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function (response) { /* promise */
+            console.log(response); 
+            let data = response;
+            parseData(data);
+            // iterate pageNum for click of 'load more' or 'next page'
+            // pageNum++;
+            renderPagination();
+        });
 }
 
 
