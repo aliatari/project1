dc9c2eeb985dc9de0d72ecde821a7333a3be9660
Spencer Jack <spencer.jack@alaskaair.com>
2018-11-27 12:50:13 -0800

    cleans up pagination functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,3 @@
-/* -------------------------------------------------------------------------------------------------- */
-/*                                                                                                    */
-/*                                        BACKGROUND IMAGES                                           */
-/*                                                                                                    */
-/* -------------------------------------------------------------------------------------------------- */
-
 // Shorthand for $( document ).ready()
 $(function () {
     // Background image array
@@ -82,19 +76,21 @@ $(function () {
     }
 })
 
-/* -------------------------------------------------------------------------------------------------- */
-/*                                                                                                    */
-/*                                             API FUNCTIONS                                          */
-/*                                                                                                    */
-/* -------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------API's Etc------------------------------------------------- */
+
 
 // globals used as param in query urls
 let page = 1;
+let movieInput = "";
 let genreID = "";
 let decadeID = "";
 let i = 0;
 let yearStart = "";
 let yearEnd = "";
+let callApiByTitle = false;
+let callApiByGenre = false;
+let pageCount = 0;
 
 // capture input values for genre
 $('#genre').on('click', 'a', function (e) {
@@ -118,7 +114,7 @@ $('#my-form').submit(function (event) {
         clearBody();
     } else {
         // grab input value from form
-        let movieInput = $('#movie-input').val().trim();
+        movieInput = $('#movie-input').val().trim();
         // pass value to function
         makeApiCall(movieInput);
         clearBody();
@@ -134,10 +130,15 @@ clearBody = function () {
 
 
 makeApiCall = function (movieInput) {
+    // iterate page count
+    // prevents double-render of pagination div
+    pageCount+=1;
+    
     // movieInput passed as a param to search movie titles
-    let queryURL = "https://api.themoviedb.org/3/search/movie?api_key=c5203bcbbee2d69dcb21052d7ef5621c&query=" + movieInput;
+    let queryURL = "https://api.themoviedb.org/3/search/movie?api_key=c5203bcbbee2d69dcb21052d7ef5621c&query=" + movieInput + "&page=" + page + "&sort_by=popularity.desc";
+
     if (movieInput === "") {
-        console.log('no movie title entered');
+        // console.log('no movie title entered');
     }
 
     $.ajax({ /* jquery ajax call */
@@ -145,58 +146,48 @@ makeApiCall = function (movieInput) {
             method: "GET"
         })
         .then(function (response) { /* promise */
-            // console.log(response); 
+            console.log(response);
             let data = response;
             parseData(data);
+            callApiByGenre = false;
+            callApiByTitle = true;
+            renderPagination();
         });
 }
 
 
 makeApiCallGenre = function (genreID, decadeID) {
-    // variables to access movies within certain decades
-    // passed to queryURL
-    // let yearStart = "";
-    // let yearEnd = "";
-
-    // define these start and end years using data-tags in the html
-    // in the drop down menu li's 
-    // this is redundant
+    // iterate page count
+    // prevents double-render of pagination div
+    pageCount+=1;
+    
     switch (true) {
-        case decadeID == "30's":
-            yearStart = "1930",
-                yearEnd = "1949"
+        case decadeID == "30's": 
+            yearStart = "1930", yearEnd = "1949"
             break
-        case decadeID == "40's":
-            yearStart = "1940",
-                yearEnd = "1949"
+        case decadeID == "40's": 
+            yearStart = "1940", yearEnd = "1949"
             break
-        case decadeID == "50's":
-            yearStart = "1950",
-                yearEnd = "1959"
+        case decadeID == "50's": 
+            yearStart = "1950", yearEnd = "1959"
             break
-        case decadeID === "60's":
-            yearStart = "1960",
-                yearEnd = "1969"
+        case decadeID === "60's": 
+            yearStart = "1960", yearEnd = "1969"
             break
-        case decadeID === "70's":
-            yearStart = "1970",
-                yearEnd = "1979"
+        case decadeID === "70's": 
+            yearStart = "1970", yearEnd = "1979"
             break
-        case decadeID === "80's":
-            yearStart = "1980",
-                yearEnd = "1989"
+        case decadeID === "80's": 
+            yearStart = "1980", yearEnd = "1989"
             break
-        case decadeID === "90's":
-            yearStart = "1990",
-                yearEnd = "1999"
+        case decadeID === "90's": 
+            yearStart = "1990", yearEnd = "1999"
             break
-        case decadeID === "00's":
-            yearStart = "2000",
-                yearEnd = "2009"
+        case decadeID === "00's": 
+            yearStart = "2000", yearEnd = "2009"
             break;
-        case decadeID === "10's":
-            yearStart = "2010",
-                yearEnd = "2019"
+        case decadeID === "10's": 
+            yearStart = "2010", yearEnd = "2019"
             break;
     }
 
@@ -209,26 +200,37 @@ makeApiCallGenre = function (genreID, decadeID) {
             method: "GET"
         })
         .then(function (response) { /* promise */
-            console.log(response); 
+            console.log(response);
             let data = response;
             parseData(data);
+            callApiByTitle = false;
+            callApiByGenre = true;
             renderPagination();
         });
 }
 
 parseData = function (data) {
-    // console.log(data);
     let base = "http://image.tmdb.org/t/p/w342/";
+
     data.results.forEach(function (element) {
-        // console.log(element);
+        let poster = "";
+
+        // if poster path returns empty
+        if (element.poster_path === null) {
+            // display stock placeholder image
+            poster = "./assets/stock-image/noPoster.jpg";
+        }else {
+            poster = base + element.poster_path;
+        }
+
         let title = element.title;
         let release_date = element.release_date;
-        let poster = base + element.poster_path;
         let overview = element.overview;
         let vote_average = element.vote_average;
         let vote_color = "";
         // converts 0-10 num to percent
         vote_average = Math.round((vote_average * 10));
+
         // constructs percentage colors
         if (vote_average <= 70) {
             vote_color = "orange";
@@ -237,19 +239,23 @@ parseData = function (data) {
         }else if (vote_average >= 80) {
             vote_color = "blue";
         }
-        // converts percent into string
+
+        // converts voting percentage into string
         vote_average = vote_average.toString();
         renderCard(title, release_date, poster, overview, vote_average, vote_color);
     });
 }
 
 renderPagination = function() {
-    let pages = $('<div id="pagination"><ul class="pagination pagination-content"><li><a href="#" class="page" id="prev">Prev</a></li><li><a href="#" class="page" id="1">1</a></li><li><a href="#" class="page" id="2">2</a></li><li><a href="#" class="page" id="3">3</a></li><li><a href="#" class="page" id="next">Next</a></li></ul></div>');
-    pages.appendTo('.container-fluid');
+    // if this is the first page render
+    if (pageCount === 1) {
+        // renders the pagination 
+        let pages = $('<div id="pagination"><ul class="pagination pagination-content"><li><a href="#" class="page" id="prev">Prev</a></li><li><a href="#" class="page" id="1">1</a></li><li><a href="#" class="page" id="2">2</a></li><li><a href="#" class="page" id="3">3</a></li><li><a href="#" class="page" id="next">Next</a></li></ul></div>');
+        pages.appendTo('.container-fluid');
+    }
 }
 
 renderCard = function (title, release_date, poster, overview, vote_average, vote_color) {
-    // $('.container-fluid').empty();
     let myCol = $('<div class="col-sm-6 col-md-6 pb-4"></div>');
 
     let myPanel = $('<div class="card card-outline-info" id="' + i + 'Panel"><div class="card-block"> <img src="' + poster + ' "class="rounded poster"> <div class="box"><div class="single-chart"><svg viewBox="0 0 36 36" class="circular-chart ' + vote_color + '"><path class="circle-bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><path class="circle" stroke-dasharray="' + vote_average + ', 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><text x="18" y="20.35" class="percentage">' + vote_average + '%</text></svg><span id="title">' + title + '<br><span id="release-date">' + release_date + '</span></span></div><div id="overview"><p class="overflow">' + overview + ' </p></div></div></div></div>');
@@ -259,37 +265,34 @@ renderCard = function (title, release_date, poster, overview, vote_average, vote
     i++;
 }
 
-// capture clicks on pagination buttons
+// captures clicks on pagination buttons
 $(document).on("click",".pagination li a", function(e){
     e.preventDefault();
+    // gets id of page button clicked
     pageInput = $(this).attr('id');
+    
+    // updates page global based on which button was clicked
     if (pageInput === 'prev') {
-        page = (page - 1);
+        page = (parseInt(page) - 1);
     }else if (pageInput === 'next') {
-        page = (page + 1);
+        page = (parseInt(page) + 1);
+    }else {
+        page = parseInt(pageInput);
     }
-    console.log(page);
-    goToPage(page);
+    goToPage();
 });
 
-goToPage = function(page) {
+goToPage = function() {
+    // empties page of previous cards
+    $('.row').empty();
     
-     // movie genreInput passed as a param to search movie genre's  
-    let queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=c5203bcbbee2d69dcb21052d7ef5621c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + page + "&primary_release_date.gte=" + yearStart + "-01-01&primary_release_date.lte=" + yearEnd + "-12-31&vote_average.gte=6&with_genres=" + genreID;
-
-
-    $.ajax({ /* jquery ajax call */
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function (response) { /* promise */
-            console.log(response); 
-            let data = response;
-            parseData(data);
-            // iterate pageNum for click of 'load more' or 'next page'
-            // pageNum++;
-            renderPagination();
-        });
+    // if user is paginating via genre's
+    if (callApiByGenre) {
+        makeApiCallGenre(genreID, decadeID);
+    // if user is paginating via title
+    }else if(callApiByTitle) {
+        makeApiCall(movieInput);
+    }
 }
 
 
